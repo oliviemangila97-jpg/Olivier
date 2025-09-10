85ac09d0407c744acb14f837c2e36d9c44302733
Olivier mangila <oliviemangila97@gmail.com>
2025-09-10 16:49:53 +0200

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,31 @@
+package com.oliviergpt.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);  
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
